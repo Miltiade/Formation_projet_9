fa997bcdd163ec264209f9b187872e36a593fb41
Miltiade <vero@tutamail.com>
2025-11-20 12:42:48 +0200

    created login & logout feature

--- litrevu/manage.py
+++ litrevu/manage.py
@@ -1,3 +1,6 @@
+
+# superutilisateur : superman
+
 #!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
--- litrevu/users/urls.py
+++ litrevu/users/urls.py
@@ -1,11 +1,12 @@
 # définir les URL pour la connexion et la déconnexion en utilisant les vues intégrées de Django
 
 from django.urls import path
+from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'users'
 
 urlpatterns = [
     path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('users:login')), name='logout'),
 ]
