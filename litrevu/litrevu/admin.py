525dbb9fdd07fb372b1a81add366dc9eac478075
Miltiade <vero@tutamail.com>
2025-11-25 13:10:08 +0200

    created models: ticket, review, userfollows

---
+++ litrevu/litrevu/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Billet, Commentaire
+
+admin.site.register(Billet)
+admin.site.register(Commentaire)
+
--- litrevu/users/models.py
+++ litrevu/users/models.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.conf import settings
+
 
 class CustomUser(AbstractUser):
     
     pass  # Ajoutez ici des champs personnalisés plus tard
+
+
+class UserFollows(models.Model):
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name="following", on_delete=models.CASCADE)
+    followed_user = models.ForeignKey(to=settings.AUTH_USER_MODEL, related_name="followed_by", on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'followed_user')
+
+    def __str__(self):
+        return f"{self.user} suit {self.followed_user}"
