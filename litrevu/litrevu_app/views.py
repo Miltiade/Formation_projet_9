73423455de4b8d57548cfb2536c24acc7968587d
Miltiade <vero@tutamail.com>
2025-12-01 11:18:12 +0200

    debugged: launching server

--- litrevu/litrevu_app/views.py
+++ litrevu/litrevu_app/views.py
@@ -6,8 +6,8 @@ from .models import Billet
 class BilletCreateView(CreateView):
     model = Billet
     fields = ['title', 'description', 'image']
-    template_name = 'litrevu/billet_form.html'
-    success_url = reverse_lazy('litrevu:flux')
+    template_name = 'litrevu_app/billet_form.html'
+    success_url = reverse_lazy('litrevu_app:flux')
 
 class HomeView(TemplateView):
-    template_name = "litrevu/home.html"
+    template_name = "litrevu_app/home.html"
