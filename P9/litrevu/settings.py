35de9cba643f40539abdf71d213ed1269811a62d
Miltiade <vero@tutamail.com>
2026-02-02 09:17:18 +0200

    added feature: redirection to flux page after login.

--- P9/litrevu/settings.py
+++ P9/litrevu/settings.py
@@ -125,7 +125,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.CustomUser"
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/billets/"
 LOGIN_URL = "/users/login/"
 
 # Filepath for media files
